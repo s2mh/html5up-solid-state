a3e82d97816b967e1743934fe97277a97343da5d
unknown <417495184@qq.com>
2025-12-16 22:37:00 +0800

    img popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,10 @@
 			small:	'(max-width: 736px)',
 			xsmall:	'(max-width: 480px)'
 		});
+		
+	// Touch?
+		if (browser.mobile)
+			$body.addClass('touch');
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
@@ -141,5 +145,41 @@
 						$menu._hide();
 
 			});
+			
+			var $gallery = $('.box.alt');
+
+			if ($gallery.length > 0) {
+				$gallery.poptrox({
+					baseZIndex: 20000,
+					caption: function($a) {
+
+						var s = '';
+
+						$a.nextAll().each(function() {
+							s += this.outerHTML;
+						});
+
+						return s;
+
+					},
+					fadeSpeed: 300,
+					onPopupClose: function() { $body.removeClass('modal-active'); },
+					onPopupOpen: function() { $body.addClass('modal-active'); },
+					overlayOpacity: 0,
+					popupCloserText: '',
+					popupHeight: 150,
+					popupLoaderText: '',
+					popupSpeed: 300,
+					popupWidth: 150,
+					selector: 'span > a.image-popup',
+					usePopupCaption: true,
+					usePopupCloser: true,
+					usePopupDefaultStyling: false,
+					usePopupForceClose: true,
+					usePopupLoader: true,
+					usePopupNav: true,
+					windowMargin: 50
+				});
+			}
 
 })(jQuery);
